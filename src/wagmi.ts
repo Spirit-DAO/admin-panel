ec5c7800f3d68894e8ccef197c59bae9da422c09
Pol <polepie@gmail.com>
2024-12-20 16:02:03 +0100

    Fix build error

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,10 +1,40 @@
-import { sonic } from 'viem/chains'
+import { defineChain } from 'viem'
 import { configureChains, createConfig } from 'wagmi'
 import { InjectedConnector } from 'wagmi/connectors/injected'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 
 import { publicProvider } from 'wagmi/providers/public'
+
+
+
+export const sonic = defineChain({
+    id: 146,
+    name: 'Sonic',
+    nativeCurrency: {
+        decimals: 18,
+        name: 'Sonic',
+        symbol: 'S',
+    },
+    rpcUrls: {
+        default: {
+            http: ['https://rpc.soniclabs.com'],
+        },
+        public: {
+            http: ["https://rpc.soniclabs.com"]
+        }
+    },
+    blockExplorers: {
+        default: { name: 'SonicScan', url: 'https://sonicscan.io/' },
+    },
+    contracts: {
+        multicall3: {
+            address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+            blockCreated: 60,
+        },
+    },
+    network: 'sonic'
+})
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [sonic],
