eb4bfab63035223b522ba9b74a5925109133f056
Pol <polepie@gmail.com>
2024-12-26 22:17:53 +0100

    Fix creation pool

--- src/constants/default-chain-id.ts
+++ src/constants/default-chain-id.ts
@@ -2,3 +2,4 @@ export const DEFAULT_CHAIN_ID = 146
 export const DEFAULT_CHAIN_NAME = 'Sonic'
 export const DEFAULT_NATIVE_SYMBOL = 'S'
 export const DEFAULT_NATIVE_NAME = 'Sonic'
+export const DEFAULT_NATIVE_WRAPPED_ADDRESS = '0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38'
--- src/hooks/common/useCurrency.ts
+++ src/hooks/common/useCurrency.ts
@@ -2,17 +2,16 @@ import { Address } from 'wagmi';
 import {
     Currency,
     ExtendedNative,
-    WNATIVE
 } from '@cryptoalgebra/integral-sdk';
 import { ADDRESS_ZERO } from "@cryptoalgebra/integral-sdk";
-import { DEFAULT_CHAIN_ID,  DEFAULT_NATIVE_NAME, DEFAULT_NATIVE_SYMBOL  } from '@/constants/default-chain-id'; 
+import { DEFAULT_CHAIN_ID,  DEFAULT_NATIVE_NAME, DEFAULT_NATIVE_SYMBOL, DEFAULT_NATIVE_WRAPPED_ADDRESS  } from '@/constants/default-chain-id'; 
 import { useAlgebraToken } from "./useAlgebraToken";
 
 export function useCurrency(
     address: Address | undefined
 ): Currency | ExtendedNative | undefined {
 
-    const isWNative = address?.toLowerCase() === WNATIVE[DEFAULT_CHAIN_ID].address.toLowerCase()
+    const isWNative = address?.toLowerCase() === DEFAULT_NATIVE_WRAPPED_ADDRESS.toLowerCase()
 
     const isNative = address === ADDRESS_ZERO;
 
